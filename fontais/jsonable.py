8cd9b32eb9a4bbe5fcdd43e3968a9943362d0c0c
Alex Wallar <aw204@st-andrews.ac.uk>
2015-07-09 19:13:30 -0400

    Added Jsonable

--- fontais/jsonable.py
+++ fontais/jsonable.py
@@ -3,5 +3,15 @@ import json
 
 
 class Jsonable(object):
+    """
+    This class should be extended for data entities that you would like
+    to make Jsonable by calling the to_json method. All variables that do
+    not start with "__" will be included in the JSON string returned by
+    the to to_json method
+    """
     def to_json(self):
-        return json.dumps(vars(self))
+        jsonable_vars = dict()
+        for key, elem in vars(self).iteritems():
+            if not key.startswith("__"):
+                jsonable_vars[key] = elem
+        return json.dumps(jsonable_vars)
